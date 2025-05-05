b4e902cf80455ca140ce790b0ee289b9d081629d
GHOKEE <145250204+GHOKEE@users.noreply.github.com>
2025-05-06 05:09:44 +0800

    Update ocr-flask.py

--- ocr-flask.py
+++ ocr-flask.py
@@ -74,4 +74,4 @@ def search_excel():
         return jsonify({"result": "CSV查询异常", "exception": str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
